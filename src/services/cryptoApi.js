c0ee1292f76226806cb628c462f680a67c339e16
Lance009 <88927806+Lance0567@users.noreply.github.com>
2024-06-15 14:03:44 +0800

    crypto currencies

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -21,25 +21,3 @@ export const cryptoApi = createApi({
 
 export const { useGetCryptosQuery, } = cryptoApi; // Corrected hook name
 
-
-// const axios = require('axios');
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd',
-//   params: {
-//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//     timePeriod: '24h'
-//   },
-//   headers: {
-    // 'x-rapidapi-key': '94e02538d7mshcc29dcc29e3a539p133f15jsn0d8f46a89a10',
-    // 'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
-//   }
-// };
-
-// try {
-// 	const response = await axios.request(options);
-// 	console.log(response.data);
-// } catch (error) {
-// 	console.error(error);
-// }
