c71bb75051856115c18f8aae08affbc56877f910
Lance009 <88927806+Lance0567@users.noreply.github.com>
2024-06-16 10:56:01 +0800

    save cryptocurrencies

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
-import millify from 'millify';
-import { Link } from 'react-router-dom';
-import { Card, Row, Col, Input } from 'antd';
+import React, { useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
 
-import { useGetCryptosQuery } from '../services/cryptoApi';
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const Cryptocurrencies = () => {
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
   const { data: cryptoList, isFetching } = useGetCryptosQuery();
   const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
 
@@ -13,14 +14,14 @@ const Cryptocurrencies = () => {
 
   return (
     <>
-      <Row gutter={[32, 32]} className='crypto-card-container'>
+      <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
-              <Card 
-              title={`${currency.rank}. ${currency.name}`}
-              extra={<img className="crypto-image" src={currency.iconUrl} />}
-              hoverable
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
               >
                 <p>Price: {millify(currency.price)}</p>
                 <p>Market Cap: {millify(currency.marketCap)}</p>
@@ -31,7 +32,7 @@ const Cryptocurrencies = () => {
         ))}
       </Row>
     </>
-  )
-}
+  );
+};
 
-export default Cryptocurrencies
+export default Cryptocurrencies;
